c71258e118eab4e884a3af1d2dfa47e47c75e8d5
Francois Marier <francois@mozilla.com>
2012-11-01 12:57:53 +1300

    Migrate the Express example to the Observer API

--- node-express/public/js/main.js
+++ node-express/public/js/main.js
@@ -1,17 +1,14 @@
-function login(event) {
-    event.preventDefault();
-    navigator.id.get(function (assertion) {
-        if (assertion) {
-            var assertion_field = document.getElementById("assertion-field");
-            assertion_field.value = assertion;
-            var login_form = document.getElementById("login-form");
-            login_form.submit();
-        }
-    });
-
-}
-
 window.onload = function () {
-  var bid = document.getElementById("browserid");
-  bid.addEventListener("click", login);
-}
+  navigator.id.watch({
+    loggedInUser: loggedInUser,
+    onlogin: function (assertion) {
+      var assertion_field = document.getElementById("assertion-field");
+      assertion_field.value = assertion;
+      var login_form = document.getElementById("login-form");
+      login_form.submit();
+    },
+    onlogout: function () {
+      window.location = '/logout';
+    }
+  });
+};
