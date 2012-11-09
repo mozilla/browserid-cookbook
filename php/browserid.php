a61016a46fd201fdaa14761b090684da98e89073
Lars Gunther <gunther@keryx.se>
2012-11-09 13:49:46 +0100

    Added comments to the PHP code
    This commit only adds comments but is not complete in that regard.
    More comments are needed.
    
    See also bug #13

--- php/browserid.php
+++ php/browserid.php
@@ -1,30 +1,45 @@
 <?php
+/**
+ * Example script that shows a very basic functionality of Persona in PHP
+ * 
+ * This script does not (yet) represent production quality code, but
+ * should provide a general overview about the steps 
+ */
+
+
 if (!empty($_POST)) {
+	// A user has attempted to log in
     $result = verify_assertion($_POST['assertion']);
     if ($result->status === 'okay') {
+    	// Login successful
         print_header();
         echo "<p>Logged in as: " . $result->email . "</p>";
         echo '<p><a href="javascript:navigator.id.logout()">Logout</a></p>';
         echo "<p><a href=\"browserid.php\">Back to login page</p>";
         print_footer($result->email);
     } else {
+    	// Login-attempt not successful
         print_header();
         echo "<p>Error: " . $result->reason . "</p>";
+        // Note that the explanation is technical and not user friendly
         echo "<p><a href=\"browserid.php\">Back to login page</p>";
         print_footer();
     }
 } elseif (!empty($_GET['logout'])) {
+	// Logout request submitted
     print_header();
     echo "<p>You have logged out.</p>";
     echo "<p><a href=\"browserid.php\">Back to login page</p>";
     print_footer();
 } else {
+	// The state of the page
     print_header();
     echo "<p><a href=\"javascript:navigator.id.request()\">Login</a>";
     print_footer();
 }
 
 function print_header() {
+	// A very simple form is being used to mimick an Ajax request
     echo <<<EOF
 <!DOCTYPE html><html><head><meta charset="utf-8"></head>
 <body>
@@ -58,6 +73,13 @@ navigator.id.watch({
 EOF;
 }
 
+/**
+ * Verify that the user has got a real asserion
+ * 
+ * @param string $assertion The assertion as received from the login dialog
+ * @param string $cabundle  Path and filename to cabundle.crt
+ * @return object
+ */
 function verify_assertion($assertion, $cabundle = NULL) {
     $audience = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'];
     $postdata = 'assertion=' . urlencode($assertion) . '&audience=' . urlencode($audience);
