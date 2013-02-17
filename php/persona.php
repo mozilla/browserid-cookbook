6c29fe7ec22ef0d60d552f048e1a2d16ef27f5a1
p.scheit <p.scheit@ps-webforge.com>
2013-02-17 11:44:14 +0100

    improvement suggestions for #13
    refactor readability
    refactor function into a Persona class
    use a php template to display the output, not functions
    fix issues when E_NOTICE is enabled
    inject audience if $_SERVER is not avaible
    indent html

--- php/persona.php
+++ php/persona.php
@@ -6,95 +6,125 @@
  * should provide a general overview of the steps.
  */
 
-if (!empty($_POST)) {
+$body = $email = NULL;
+if (isset($_POST['assertion'])) { // @TODO security: what is assertion here?
+    $persona = new Persona();
+    
     // A user has attempted to log in
-    $result = verify_assertion($_POST['assertion']);
+    $result = $persona->verifyAssertion($_POST['assertion']);
+    
     if ($result->status === 'okay') {
-        // Login successful
-        print_header();
-        echo "<p>Logged in as: " . $result->email . "</p>";
-        echo '<p><a href="javascript:navigator.id.logout()">Logout</a></p>';
-        print_backLink();
-        print_footer($result->email);
+        $body = "<p>Logged in as: " . $result->email . "</p>";
+        $body .= '<p><a href="javascript:navigator.id.logout()">Logout</a></p>';
+        $body .= html_backLink();
+        $email = $result->email;
+        
     } else {
         // Login-attempt not successful
-        print_header();
-        echo "<p>Error: " . $result->reason . "</p>";
+        
         // Note that the explanation is technical and not user friendly
-        print_backLink();
-        print_footer();
+        $body = "<p>Error: " . $result->reason . "</p>";
+        
+        $body .= html_backLink();
     }
 } elseif (!empty($_GET['logout'])) {
-    // Logout request submitted
-    print_header();
-    echo "<p>You have logged out.</p>";
-    print_backLink();
-    print_footer();
+    $body = "<p>You have logged out.</p>";
+    $body .= html_backLink();
 } else {
     // The state of the page
-    print_header();
-    echo "<p><a href=\"javascript:navigator.id.request()\">Login</a></p>";
-    print_footer();
+    $body = "<p><a href=\"javascript:navigator.id.request()\">Login</a></p>";
 }
 
-function print_header() {
-    // A very simple form is being used to mimick an Ajax request
-    echo <<<EOF
-<!DOCTYPE html><html><head><meta charset="utf-8"></head>
-<body>
-<form id="login-form" method="POST">
-<input id="assertion-field" type="hidden" name="assertion" value="">
-</form>
-EOF;
+function html_backLink() {
+    return "<p><a href=\"persona.php\">Back to login page</a></p>";
 }
 
-function print_backLink() {
-    echo "<p><a href=\"persona.php\">Back to login page</a></p>";
-}
+// A very simple form is being used to mimick an Ajax request
+?><!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <form id="login-form" method="POST">
+      <input id="assertion-field" type="hidden" name="assertion" value="">
+    </form>
+    <?= $body ?>
+    <script src="https://login.persona.org/include.js"></script>
+    <script>
+    navigator.id.watch({
+        loggedInUser: <?= $email ? "'$email'" : 'null' ?>,
+        onlogin: function (assertion) {
+            var assertion_field = document.getElementById("assertion-field");
+            assertion_field.value = assertion;
+            var login_form = document.getElementById("login-form");
+            login_form.submit();
+        },
+        onlogout: function () {
+            window.location = '?logout=1';
+        }
+    });
+    </script>
+  </body>
+</html>
+<?php
 
-function print_footer($email = 'null') {
-    if ($email !== 'null') {
-        $email = "'$email'";
+class Persona
+{
+    /**
+     * Audience full ServerName
+     * 
+     * @var string including http:// or https:// including : and port
+     */
+    protected $audience;
+
+    /**
+     * constructs a new Persona
+     *
+     * if $audience is not provided the audience will be guessed from $_SERVER
+     * @param string $audience including http:// or https:// including : and port
+     */
+    public function __construct($audience = NULL, $serverPort = NULL)
+    {
+        $this->audience = $audience ?: $this->guessAudience();
     }
-    echo <<<EOF
-<script src="https://login.persona.org/include.js"></script>
-<script>
-navigator.id.watch({
-    loggedInUser: $email,
-    onlogin: function (assertion) {
-        var assertion_field = document.getElementById("assertion-field");
-        assertion_field.value = assertion;
-        var login_form = document.getElementById("login-form");
-        login_form.submit();
-    },
-    onlogout: function () {
-        window.location = '?logout=1';
+  
+    /**
+     * Verify that the user has got a real asserion
+     *
+     * returns the response from the persona verifier service
+     * @param string $assertion The assertion as received from the login dialog
+     * @return object
+     */
+    public function verifyAssertion($assertion)
+    {
+        $postdata = 'assertion=' . urlencode($assertion) . '&audience=' . urlencode($this->audience);
+    
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://verifier.login.persona.org/verify");
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        $response = curl_exec($ch);
+        curl_close($ch);
+    
+        return json_decode($response);
     }
-});
-</script>
-</body></html>
-EOF;
+    
+    /**
+     * Guesses an Audience from global $_SERVER vars
+     *
+     * @return string the full server name with port an http://
+     */
+    protected function guessAudience()
+    {
+        $audience = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+        $audience .= $_SERVER['SERVER_NAME'];
+        $audience .= ':'.$_SERVER['SERVER_PORT'];
+        
+        return $audience;
+    }
 }
-
-/**
- * Verify that the user has got a real asserion
- *
- * @param string $assertion The assertion as received from the login dialog
- * @return object
- */
-function verify_assertion($assertion) {
-    $audience = ($_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'];
-    $postdata = 'assertion=' . urlencode($assertion) . '&audience=' . urlencode($audience);
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://verifier.login.persona.org/verify");
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-    $json = curl_exec($ch);
-    curl_close($ch);
-
-    return json_decode($json);
-}
+?>
