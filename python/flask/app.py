a9c7b4eeb1864aa5d15a93f0d8c1fd9b656d1ac2
Cesar Carruitero <cesar@mozilla.pe>
2013-04-12 03:34:23 -0500

    flask demo

---
+++ python/flask/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, abort, redirect, render_template, request, session
+import requests
+import json
+
+app = Flask(__name__)
+app.secret_key= 'tumama!!'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/auth/login', methods=["POST"])
+def login():
+    if 'assertion' not in request.form:
+        abort(400)
+
+    assertion_info = {'assertion': request.form['assertion'],
+                        'audience': 'localhost:5000' } # window.location.host
+    resp = requests.post('https://verifier.login.persona.org/verify',
+                        data=assertion_info, verify=True)
+
+    if not resp.ok:
+        abort(500)
+
+    data = json.loads(resp.content)
+
+    if data['status'] == 'okay':
+        session.update({'email': data['email']})
+        return resp.content
+
+@app.route('/auth/logout', methods=["POST"])
+def logout():
+    session.pop('email', None)
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
